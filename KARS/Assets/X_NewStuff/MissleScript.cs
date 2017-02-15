363822dfc7a8947fbcf3e9e86ab6f721b687bc54
Burlin S. Gauna <burlin_gauna@yahoo.com>
2017-02-15 17:54:15 +0800

    ADDED plugin for FPS

---
+++ KARS/Assets/X_NewStuff/MissleScript.cs
@@ -0,0 +1,189 @@
+﻿using GameSparks.RT;
+using Synergy88;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissleScript : MonoBehaviour {
+    public struct State
+    {
+        internal Vector3 pos;
+        internal Vector3 rot;
+    }
+
+    public State[] m_BufferedState = new State[20];
+    //================================================================================================================================
+    #region VARIABLES
+    [SerializeField]
+    private int _playerController_ID;
+    public int PlayerController_ID
+    {
+        get { return _playerController_ID; }
+        set { _playerController_ID = value; }
+    }
+
+    [SerializeField]
+    private int missle_ID;
+    public int Missle_ID
+    {
+        get { return missle_ID; }
+        set { missle_ID = value; }
+    }
+
+
+    [SerializeField]
+    private GameObject objectToHit;
+
+    [SerializeField]
+    private bool lockOnObject;
+
+    public Transform missleParent;
+    float missleSpeed = 0.5f;
+
+    private GameSparksRTUnity GetRTSession;
+
+    public Vector3 SyncMovement;
+    public Vector3 SyncRot;
+    #endregion
+    //================================================================================================================================
+    #region UPDATE AND SYNC
+    void Update()
+    {
+        if(TronGameManager.Instance.NetworkStart == false)
+        {
+            transform.position += transform.forward * 1f;
+            return;
+        }
+
+
+        if (_playerController_ID == GameSparkPacketReceiver.Instance.PeerID)
+        {
+            //Debug.LogError("missle is controlling");
+            transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.blue;
+            if (lockOnObject)
+            {
+                if (Vector3.Distance(transform.position, objectToHit.transform.position) < 1)
+                {
+                    SendToSErverTheCollision();
+                }
+                else
+                {
+                    SendMissleData(1);
+                    transform.position += transform.forward * 1f; //Vector3.MoveTowards(transform.position, objectToHit.transform.position, missleSpeed);
+                    //transform.LookAt(objectToHit.transform.position);
+                }
+            }
+        }
+        else
+        {
+            //Debug.LogError("missle is syncing");
+            transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.red;
+            transform.position = Vector3.Lerp(transform.position, SyncMovement, 1);
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, SyncRot, 1);
+        }
+    }
+    public void SetSYnc(Vector3 _pos,Vector3 _rot)
+    {
+        for (int q = m_BufferedState.Length - 1; q >= 1; q--)
+        {
+            m_BufferedState[q] = m_BufferedState[q - 1];
+        }
+
+        State state;
+        state.pos = _pos;
+        state.rot =_rot;
+        m_BufferedState[0] = state;
+
+
+        try
+        {
+            SyncMovement = m_BufferedState[0].pos + (m_BufferedState[0].pos - m_BufferedState[1].pos);
+        }
+        catch
+        {
+            SyncMovement = _pos;
+        }
+        SyncRot = _rot;
+    }
+    #endregion
+    //================================================================================================================================
+    #region SEND DATA
+    void SendMissleData(int _var)
+    {
+        try
+        {
+            GetRTSession = GameSparkPacketReceiver.Instance.GetRTSession();
+            using (RTData data = RTData.Get())
+            {
+                data.SetInt(1, missle_ID);
+                data.SetInt(2, PlayerController_ID);
+                data.SetFloat(3, transform.position.x);
+                data.SetFloat(4, transform.position.y);
+                data.SetFloat(5, transform.position.z);
+                data.SetVector3(6, transform.eulerAngles);
+                data.SetInt(7, _var);
+
+                GetRTSession.SendData(115, GameSparksRT.DeliveryIntent.UNRELIABLE_SEQUENCED, data);
+            }
+        }
+        catch { }
+    }
+    #endregion
+    //================================================================================================================================
+    #region PUBLIC FUNCTIONS
+    public void Set_MissleID(int _var)
+    {
+        missleParent = transform.parent;
+        lockOnObject = false;
+        missle_ID = _var;
+        gameObject.name += _var;
+        gameObject.SetActive(false);
+    }
+    public void LockOnToThisObject(GameObject _sender,GameObject _obj)
+    {
+        //GameObject.Find("GameUpdateText").GetComponent<Text>().text += "\nMissle "+gameObject.name+" Locking on to: "+_obj.name;
+       // Debug.LogError("locking on to object "+_obj.gameObject.name);
+        transform.position = _sender.transform.position;
+        transform.rotation = _sender.GetComponent<Car_Movement>().CarRotationObject.transform.rotation;
+        objectToHit = _obj;
+        transform.SetParent(null);
+        lockOnObject = true;
+        gameObject.SetActive(true);
+    }
+    public void ResetMissle()
+    {
+        transform.SetParent(missleParent);
+        transform.position = missleParent.transform.position;
+        SendMissleData(0);
+        objectToHit = null;
+        lockOnObject = false;
+        gameObject.SetActive(false);
+    }
+    #endregion
+    //================================================================================================================================
+    void SendToSErverTheCollision()
+    {
+        try
+        {
+
+            if (objectToHit.GetComponent<Car_DataReceiver>()._shieldSwitch)
+            {
+                ResetMissle();
+                return;
+            }
+
+            GetRTSession = GameSparkPacketReceiver.Instance.GetRTSession();
+            using (RTData data = RTData.Get())
+            {
+                data.SetInt(1, objectToHit.GetComponent<Car_DataReceiver>().Network_ID);
+                data.SetInt(2, 2);
+
+                GetRTSession.SendData(117, GameSparksRT.DeliveryIntent.UNRELIABLE_SEQUENCED, data);
+                //GameObject.Find("GameUpdateText").GetComponent<Text>().text += "\nServer (" + GameSparkPacketReceiver.Instance.PeerID + ") Owner (" + _playerController_ID + "Missle # " + Missle_ID + " hit " + hit.gameObject.name;
+            }
+            ResetMissle();
+        }
+        catch { }
+    }
+}
---
+++ KARS/Assets/X_NewStuff/PowerUpManager.cs
@@ -0,0 +1,304 @@
+﻿using GameSparks.RT;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpManager : MonoBehaviour {
+    //=======================================================================================================================
+    //VARIABLES
+    #region VARIABLES
+    private static PowerUpManager instance;
+    public static PowerUpManager Instance
+    {
+        get { return instance; }
+    }
+
+    int ServerPeerID;
+
+    [SerializeField]
+    GameObject Player1, Player2;
+    public GameObject Shield;
+
+    [SerializeField]
+    Transform MisslePool_Player1, MisslePool_Player2, TnTPool;
+
+    public List<GameObject> MissleList_Player1;
+    public List<GameObject> MissleList_Player2;
+    public List<GameObject> TnTList;
+    private GameSparksRTUnity GetRTSession;
+    #endregion
+    //=======================================================================================================================
+    //INITIALIZATION
+    #region INITIALIZATION
+    void Awake()
+    {
+        instance = this;
+        Player1 = GameObject.Find("Car1");
+        Player2 = GameObject.Find("Car2");
+    }
+
+    public void StartNetwork()
+    {
+        MissleList_Player1 = new List<GameObject>();
+        MissleList_Player2 = new List<GameObject>();
+        TnTList = new List<GameObject>();
+        StartCoroutine(DelayStartup());
+    }
+
+    IEnumerator DelayStartup()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject temp = null;
+        
+        for (int i = 0; i < MisslePool_Player1.childCount; i++)
+        {
+            temp = MisslePool_Player1.GetChild(i).gameObject;
+            MissleList_Player1.Add(temp);
+            temp.GetComponent<MissleScript>().Set_MissleID(i);
+        }
+        for (int i = 0; i < MisslePool_Player2.childCount; i++)
+        {
+            temp = MisslePool_Player2.GetChild(i).gameObject;
+            MissleList_Player2.Add(temp);
+            temp.GetComponent<MissleScript>().Set_MissleID(i);
+        }
+
+
+        yield return new WaitForSeconds(1);
+        temp = null;
+        for (int i = 0; i < TnTPool.childCount; i++)
+        {
+            temp = TnTPool.GetChild(i).gameObject;
+            TnTList.Add(temp);
+            temp.GetComponent<TnTScript>().InitializeObj(i);
+        }
+        Debug.LogError("PEER SETUP");
+        try
+        {
+            ServerPeerID = (GameSparkPacketReceiver.Instance.PeerID);
+        }
+        catch { }
+    }
+    #endregion
+    //=======================================================================================================================
+    //TNT SEND AND RECEIVE FROM SERVER
+    #region TNT SEND AND RECEIVE FROM SERVER
+    public void SetUpTNT(int _id, Vector3 _pos, bool _enable)
+    {
+        if(TronGameManager.Instance.NetworkStart == false)
+        {
+             try
+            {
+                GameObject temp = TnTPool.transform.GetChild(0).gameObject;
+                temp.GetComponent<TnTScript>().DispatchTNTToDestination(_id, _pos, _enable);
+            }
+            catch
+            {
+                GameObject temp = TnTList[0];
+                for (int i = 0; i < TnTList.Count - 1; i++)
+                {
+                    TnTList[i] = TnTList[i + 1];
+                }
+                TnTList[TnTList.Count - 1] = temp;
+                TnTList[TnTList.Count - 1].GetComponent<TnTScript>().ResetTnT();
+                SetUpTNT(_id, _pos, _enable);
+            }
+
+            return;
+        }
+
+
+        try
+        {
+            GameObject temp = TnTPool.transform.GetChild(0).gameObject;
+            temp.GetComponent<TnTScript>().DispatchTNTToDestination(_id, _pos, _enable);
+            SendToServer(_id, temp.GetComponent<TnTScript>().TNT_ID, _pos, _enable);
+        }
+        catch
+        {
+            GameObject temp = TnTList[0];
+            for (int i = 0; i < TnTList.Count - 1; i++)
+            {
+                TnTList[i] = TnTList[i + 1];
+            }
+            TnTList[TnTList.Count - 1] = temp;
+            TnTList[TnTList.Count - 1].GetComponent<TnTScript>().ResetTnT();
+            SetUpTNT(_id,_pos,_enable);
+        }
+    }
+
+    public void ReceiveFromServer(int _id, int _tntID, Vector3 _pos, bool _enable)
+    {
+        GameObject.Find("GameUpdateText").GetComponent<Text>().text += "\nReceived From Server";
+        for(int i = 0; i < TnTList.Count; i++)
+        {
+            if(TnTList[i].GetComponent<TnTScript>().TNT_ID == _tntID)
+            {
+                TnTList[i].GetComponent<TnTScript>().DispatchTNTToDestination(_id,_pos,_enable);
+            }
+        }
+    }
+
+    public void SendToServer(int _id, int _tntID,Vector3 _pos, bool _enable)
+    {
+        GetRTSession = GameSparkPacketReceiver.Instance.GetRTSession();
+        using (RTData data = RTData.Get())
+        {
+            data.SetInt(1, _id);
+            data.SetInt(2, _tntID);
+            data.SetVector3(3, _pos);
+            if (_enable)
+                data.SetInt(4, 1);
+            else
+                data.SetInt(4, 0);
+
+            GetRTSession.SendData(116, GameSparksRT.DeliveryIntent.UNRELIABLE_SEQUENCED, data);
+        }
+    }
+    #endregion
+    //=======================================================================================================================
+    //MISSLE SEND AND RECEIVE FROM SERVER
+    #region MISSLE SEND AND RECEIVE FROM SERVER
+    public void LockOnTarget(int senderID,GameObject _obj)
+    {
+        if(senderID == 1 || senderID == 0)
+        {
+            MissleList_Player1[0].GetComponent<MissleScript>().LockOnToThisObject(Player1,_obj);
+            GameObject temp = MissleList_Player1[0];
+            for (int i = 0; i < MissleList_Player1.Count - 1; i++)
+            {
+                MissleList_Player1[i] = MissleList_Player1[i + 1];
+            }
+            MissleList_Player1[MissleList_Player1.Count - 1] = temp;
+        }
+        else if (senderID == 2)
+        {
+            MissleList_Player2[0].GetComponent<MissleScript>().LockOnToThisObject(Player2,_obj);
+
+            GameObject temp = MissleList_Player2[0];
+            for (int i = 0; i < MissleList_Player2.Count - 1; i++)
+            {
+                MissleList_Player2[i] = MissleList_Player2[i + 1];
+            }
+            MissleList_Player2[MissleList_Player2.Count - 1] = temp;
+        }
+    }
+    /*
+    void ClientSendToServerMissleLock(int sender, int receiver)
+    {
+        GetRTSession = GameSparkPacketReceiver.Instance.GetRTSession();
+        using (RTData data = RTData.Get())
+        {
+            data.SetInt(1, sender);
+            data.SetInt(2, receiver);
+            GetRTSession.SendData(114, GameSparksRT.DeliveryIntent.UNRELIABLE_SEQUENCED, data);
+        }
+    }*/
+    #endregion
+    //=======================================================================================================================
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LaunchMissleFromBUtton();
+        }
+        if(Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            DropTNTFromButton();
+        }
+
+
+        return;
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            if (GameSparkPacketReceiver.Instance.PeerID == 2)
+            {
+                GetRTSession = GameSparkPacketReceiver.Instance.GetRTSession();
+                using (RTData data = RTData.Get())
+                {
+                    data.SetInt(1, 1);
+                    data.SetInt(2, 1);
+
+                    GetRTSession.SendData(117, GameSparksRT.DeliveryIntent.UNRELIABLE_SEQUENCED, data);
+                }
+            }
+            else if (GameSparkPacketReceiver.Instance.PeerID == 1)
+            {
+                GetRTSession = GameSparkPacketReceiver.Instance.GetRTSession();
+                using (RTData data = RTData.Get())
+                {
+                    data.SetInt(1,2);
+                    data.SetInt(2, 1);
+
+                    GetRTSession.SendData(117, GameSparksRT.DeliveryIntent.UNRELIABLE_SEQUENCED, data);
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+
+            GameObject.Find("GameUpdateText").GetComponent<Text>().text = "";
+        }
+    }
+
+    public void LaunchMissleFromBUtton()
+    {
+        if (GameSparkPacketReceiver.Instance.PeerID == 2)
+        {
+            LockOnTarget(2, Player1);
+        }
+        else if (GameSparkPacketReceiver.Instance.PeerID == 1)
+        {
+            LockOnTarget(1, Player2);
+        }
+        else
+        {
+            LockOnTarget(0,Player2);
+        }
+    }
+
+    internal void LaunchMissle()
+    {
+        if (GameSparkPacketReceiver.Instance.PeerID == 2)
+        {
+            LockOnTarget(2, Player1);
+        }
+        else if (GameSparkPacketReceiver.Instance.PeerID == 1)
+        {
+            LockOnTarget(1, Player2);
+        }
+    }
+
+    public void DropTNTFromButton()
+    {
+
+        if (GameSparkPacketReceiver.Instance.PeerID == 2)
+        {
+            SetUpTNT(2, Player2.transform.position, true);
+        }
+        else if (GameSparkPacketReceiver.Instance.PeerID == 1)
+        {
+            SetUpTNT(1, Player1.transform.position, true);
+        }
+        else
+        {
+            SetUpTNT(0, Player1.transform.position, true);
+        }
+    }
+    internal void DropTNT()
+    {
+        Debug.LogError("Drop TNT");
+
+        if (GameSparkPacketReceiver.Instance.PeerID == 2)
+        {
+            SetUpTNT(2, Player2.transform.position, true);
+        }
+        else if (GameSparkPacketReceiver.Instance.PeerID == 1)
+        {
+            SetUpTNT(1, Player1.transform.position, true);
+        }
+    }
+
+}

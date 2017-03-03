78ccd74b6742acb9c16fe5fd2f10a3a43594c5b7
Burlin S. Gauna <burlin_gauna@yahoo.com>
2017-03-03 14:56:27 +0800

    added turning tweaker

---
+++ KARS/Assets/VeloSlider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VeloSlider : MonoBehaviour
+{
+    public Car_Movement obj;
+    void Update()
+    {
+        GetComponent<Slider>().value = -obj.CurrentTurningForceRatio;
+    }
+}
